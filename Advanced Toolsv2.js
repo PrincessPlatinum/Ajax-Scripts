0457134680dc6664c4cd197ffc51404c0e466d7b
Princess Platinum (Wikia Username) <princess.platinum.wikia@gmail.com>
2013-09-20 09:53:23 -0400

    Update Advanced Toolsv2.js

--- Advanced Toolsv2.js
+++ Advanced Toolsv2.js
@@ -1,17 +1,46 @@
-(function(window, $, mw) {
-    //Get the jQuery UI
-    $.getScript('https://raw.github.com/PrincessPlatinum/Ajax-Scripts/master/jQuery-UI.js');
+(function (window, $, mw) {
     //Global Variables
-    var pageName = mw.config.get( "wgPageName" ),
-        server = mw.config.get( "wgServer" ),
+    var pageName = mw.config.get("wgPageName"),
+        server = mw.config.get("wgServer"),
         token = mw.user.tokens.values.editToken,
-        pageId = mw.config.get( "wgArticleId" ),
-        namespace = mw.config.get( "wgNamespaceNumber" ),
-        signature = "~~" + "~~";
+        pageId = mw.config.get("wgArticleId"),
+        namespace = mw.config.get("wgNamespaceNumber"),
+        signature = "~~" + "~~",
+        mode;
 
     //Un-urlencoding pagename
     pageName = pageName.replace(/_/g, ' ');
 
     //Main menu modal
-    
+
+    function mainMenu() {
+        $.showCustomModal("Advanced Tools", '<form class="WikiaForm" method="" name=""><fieldset><div style="text-align: center;"><a class="wikia-button" onclick="function(){mode=\'block\';}">Block user</a>&nbsp;<a class="wikia-button" onclick="function(){mode=\'delete\';}">Delete page</a>&nbsp;<a class="wikia-button" onclick="function(){mode=\'move\';}">Move page</a>&nbsp;<a class="wikia-button" onclick="function(){mode=\'protect\';}">(Un)Protect page</a>&nbsp;<a class="wikia-button" onclick="function(){mode=\'purge\';}">Purge page</a><br><br><a class="wikia-button" onclick="function(){mode=\'redirect\';}">Redirect page</a>&nbsp;<a class="wikia-button" onclick="function(){mode=\'redlinks\';}">Remove redlinks</a>&nbsp;<a class="wikia-button" href="/index.php?title=' + encodeURIComponent(wgPageName) + '&amp;useskin=monobook">Switch skin</a>&nbsp;<a class="wikia-button" onclick="function(){mode=\'template\';}">Add template</a>&nbsp;<a class="wikia-button" onclick="function(){mlde=\'unsigned\';}">Add unsigned</a>&nbsp;<a class="wikia-button" onclick="function(){mode=\'batch\';}">Batch delete</a></div></fieldset></form>', {
+            id: "advancedtools",
+            width: 650,
+            buttons: [{
+                id: "close-menu",
+                defaultButton: true,
+                message: "Close form",
+                handler: function () {
+                    $("#advancedtools").closeModal();
+                }
+            }]
+        });
+ 
+        //Switch function (more compact?)
+        switch(mode) {
+            case "block":
+                modal("block");
+                break;
+            case "delete":
+                modal("delete");
+                break;
+            case "move":
+                modal("move");
+                break;
+            case "protect":
+                modal("protect");
+                break;
+        }
+    }
 }(this, this.jQuery, this.mediaWiki));
