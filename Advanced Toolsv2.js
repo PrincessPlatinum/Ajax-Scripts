c8cf1d08812712eda4ba7e63901c882f5f7cd3e8
Princess Platinum (Wikia Username) <princess.platinum.wikia@gmail.com>
2013-09-22 14:42:53 -0400

    Update Advanced Toolsv2.js

--- Advanced Toolsv2.js
+++ Advanced Toolsv2.js
@@ -60,7 +60,7 @@
             modal("delete");
         });
         $("#at-move").click(function () {
-            modal("move")
+            modal("move");
         });
         $("#at-protect").click(function () {
             modal("protect");
@@ -69,7 +69,7 @@
             function(){var page=encodeURIComponent(page_name);$.get('/index.php?title='+page+'&action=purge',function(){location.reload(true);});}
         });
         $("#at-redirect").click(function () {
-            modal("redirect")
+            modal("redirect");
         });
         $("#at-redlinks").click(function () {
             function(){$.getJSON('/api.php?action=query&prop=revisions&titles='+encodeURIComponent(wgPageName)+'&rvprop=content&indexpageids=1&format=json',function(result){var text=result.query.pages[result.query.pageids[0]].revisions[0]['*'];var redlinks=$('a.new');var ref=0;while(text.indexOf('[[',ref)!=-1){if(text.indexOf(']]',text.indexOf('[[',ref))!=-1){var open=text.indexOf('[[',ref);var pipe=text.indexOf('|',open);var close=text.indexOf(']]',open);if(text.indexOf('<br/>',open)!=-1&&text.indexOf('<br/>',open)<close){ref=open+1;continue;}if(pipe!=-1&&pipe<close){if(pipe==close-1){var title=text.substring(open+2,pipe);var display=title.substring(title.indexOf(':')+1);}else{var title=text.substring(open+2,pipe);var display=text.substring(pipe+1,close);}}else{var title=text.substring(open+2,close);var display=title;}if(!title){ref=open+1;continue;}for(var i=0;i<redlinks.length;i++){var utitle=title[0].toUpperCase()+title.substring(1);if(redlinks[i].title.indexOf(' (page does not exist)')){var redlink=redlinks[i].title.substring(0,redlinks[i].title.length-22);}else{var redlink=redlinks[i];}if(utitle==redlink||utitle==':'+redlink){if(pipe==close-1){var show=title;}else{var show=display;}text=text.substring(0,open)+show+text.substring(close+2);break;}}ref=open+2;}else{break;}}$.getJSON('/api.php?action=query&prop=info&titles='+encodeURIComponent(wgPageName)+'&intoken=edit&indexpageids=1&format=json',function(result){$.post('/api.php',{action:'edit',title:wgPageName,text:text,summary:'Fixing redlinks',token:EditToken},function(){window.location.reload();});});});}
@@ -103,39 +103,39 @@
             modal_function_to_call = function() { var page = document.getElementById('delete-page').value, reason = document.getElementById('delete-reason').value; ajaxDelete(page, reason); };
             break;
         case "move":
-            modal_title = ;
-            modal_html = ;
-            modal_id = ;
-            modal_button_name = ;
-            modal_function_to_call = ;
+            modal_title;
+            modal_html;
+            modal_id;
+            modal_button_name;
+            modal_function_to_call;
             break;
         case "protect":
-            modal_title = ;
-            modal_html = ;
-            modal_id = ;
-            modal_button_name = ;
-            modal_function_to_call = ;
+            modal_title;
+            modal_html;
+            modal_id;
+            modal_button_name;
+            modal_function_to_call;
             break;
         case "redirect":
-            modal_title = ;
-            modal_html = ;
-            modal_id = ;
-            modal_button_name = ;
-            modal_function_to_call = ;
+            modal_title;
+            modal_html;
+            modal_id;
+            modal_button_name;
+            modal_function_to_call;
             break;
         case "template":
-            modal_title = ;
-            modal_html = ;
-            modal_id = ;
-            modal_button_name = ;
-            modal_function_to_call = ;
+            modal_title;
+            modal_html;
+            modal_id;
+            modal_button_name;
+            modal_function_to_call;
             break;
-        case "batch";
-            modal_title = ;
-            modal_html = ;
-            modal_id = ;
-            modal_button_name = ;
-            modal_function_to_call = ;
+        case "batch":
+            modal_title;
+            modal_html;
+            modal_id;
+            modal_button_name;
+            modal_function_to_call;
         break;
         default:
             alert("Error encountered!");
