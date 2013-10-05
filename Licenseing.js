49c77e65fbc9a2bad09e3470d84a50eed186655f
Princess Platinum (Wikia Username) <princess.platinum.wikia@gmail.com>
2013-10-05 09:42:32 -0400

    Update Licenseing.js

--- Licenseing.js
+++ Licenseing.js
@@ -15,7 +15,7 @@
         });
         $('#mw-diff-ntitle1').find('.mw-rev-head-action').append($(node));
     }
-    mw.util.addCSS('#ajax-revert:before{content:" (";color:inherit;}#ajax-revert:after{content:") ";color:inherit;}');
+    mw.util.addCSS('#ajax-revert:before{content:"<br /> (";color:inherit;}#ajax-revert:after{content:") ";color:inherit;}');
 
     function continueOperation() {
         (new mw.Api()).get({
